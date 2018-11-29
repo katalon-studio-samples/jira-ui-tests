fc102bd64ca7682f4f45dcf3a9832080bd86b491
trongbui <trongbui@SGH812TXHL.kms.com.vn>
2018-11-29 18:42:04 +0700

    commit from KS

--- Test Suites/Regression Tests/Regression Tests.ts
+++ Test Suites/Regression Tests/Regression Tests.ts
@@ -4,7 +4,7 @@
    <name>Regression Tests</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-08-01T15:28:54</lastRun>
+   <lastRun>2018-11-29T18:35:44</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
