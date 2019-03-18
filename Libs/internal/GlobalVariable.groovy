23499c11727e7dd27abedaf6526515c45aa6eb6e
Brian Ducson <brian.ducson@gmail.com>
2019-03-18 09:58:15 +0700

    Update properties

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -1,12 +1,8 @@
 package internal
 
 import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import com.kms.katalon.core.main.TestCaseMain
+
 
 /**
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
@@ -45,27 +41,20 @@ public class GlobalVariable {
      
 
     static {
-        def allVariables = [:]        
-        allVariables.put('default', ['URL' : 'https://katalon.atlassian.net', 'password' : 'sPiHQ&YEa6ST`de+', 'encrypted_password' : 'KLAk0b2rAgvA1EV7zgpKiS/uV+5nc48Y', 'username' : 'demo@katalon.com', 'element_timeout' : 60, 'ticket_sample' : 'Ticket created at 1528442112968'])
-        allVariables.put('dev profile', allVariables['default'] + ['username' : 'bella'])
-        allVariables.put('qa profile', allVariables['default'] + ['username' : 'tom'])
-        
-        String profileName = RunConfiguration.getExecutionProfile()
-        def selectedVariables = allVariables[profileName]
-		
-		for(object in selectedVariables){
-			String overridingGlobalVariable = RunConfiguration.getOverridingGlobalVariable(object.key)
-			if(overridingGlobalVariable != null){
-				selectedVariables.put(object.key, overridingGlobalVariable)
-			}
-		}
-
-        URL = selectedVariables["URL"]
-        password = selectedVariables["password"]
-        encrypted_password = selectedVariables["encrypted_password"]
-        username = selectedVariables["username"]
-        element_timeout = selectedVariables["element_timeout"]
-        ticket_sample = selectedVariables["ticket_sample"]
-        
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            URL = selectedVariables['URL']
+            password = selectedVariables['password']
+            encrypted_password = selectedVariables['encrypted_password']
+            username = selectedVariables['username']
+            element_timeout = selectedVariables['element_timeout']
+            ticket_sample = selectedVariables['ticket_sample']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
     }
 }
